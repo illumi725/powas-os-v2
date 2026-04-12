62af543957f98fba3694fa1541b4f5492ddb74bc
POWAS-OS <itreports06194@gmail.com>
2026-04-12 13:51:14 +0800

    fix: unscrollable after modal actions

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -15,6 +15,13 @@ $maxWidth = [
 
 <div
     x-data="{ show: @entangle($attributes->wire('model')) }"
+    x-init="$watch('show', value => {
+        if (value) {
+            document.body.style.overflow = 'hidden';
+        } else {
+            document.body.style.overflow = '';
+        }
+    })"
     x-on:close.stop="show = false"
     x-on:keydown.escape.window="show = false"
     x-show="show"
@@ -32,7 +39,7 @@ $maxWidth = [
     </div>
 
     <div x-show="show" class="mb-6 bg-white dark:bg-gray-800 rounded-lg overflow-hidden shadow-xl transform transition-all sm:w-full {{ $maxWidth }} sm:mx-auto"
-                    x-trap.inert.noscroll="show"
+                    x-trap.inert="show"
                     x-transition:enter="ease-out duration-300"
                     x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                     x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
